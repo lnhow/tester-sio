3ae4cf3e35df19a806d8d4a28eaf372fa65e4480
Hao <47709398+lnhow@users.noreply.github.com>
2023-04-04 13:27:34 +0700

    main update

--- index.js
+++ index.js
@@ -4,39 +4,38 @@ const createSocketIOConnection = require('./socket-io.create')
 const argv = process.argv.slice(2)
 const connectArgv = argv.length > 0 ? parseInt(argv[0]) : null
 
-const n_connection = connectArgv || 15000 // 15000
-// const n_connection = 10000
-console.log(n_connection)
 const isDirect = true
-const isStaging = false
+// const isStaging = false
 
 const handshakePathDirect = '/api/skijan-vi/ws/handshake'
 const handshakePathProxy = '/vi/api/skijan-vi/ws/handshake'
-const handshakePath = isDirect ? handshakePathDirect : handshakePathProxy
+const handshakePath = handshakePathDirect //isDirect ?  : handshakePathProxy
 
 const namespace = 'ws/skijan-vi/test'
 
-const stgUrl = `https://skijan.gogojungle.com/${namespace}`
-const localUrl = `ws://localhost:8925/${namespace}`
+const stgUrl = `https://video.gogojungle.net/${namespace}`
+const localUrl = `ws://localhost:8927/${namespace}`
 const proxyLocalUrl = `ws://localhost:8920/${namespace}`
-const url = isDirect ? localUrl : (isStaging ? stgUrl : proxyLocalUrl)
+const url = isDirect ? localUrl : stgUrl
 
 const wsOptions = {
   path: handshakePath,
   forceNew: true,
-  reconnection: true,
+  reconnection: false,
+  timeout: 40000,
   reconnectionDelay: 15000,
   // reconnectionDelayMax: 5000,
-  reconnectionAttempts: 3,
+  // reconnectionAttempts: 3,
   transports: ['websocket'],
 }
 
 
-async function test(n_connection = 10000) {
+async function test(n_connection = 10000, steps = 100) {
+  console.log(n_connection)
   let i = 0;
   while (i < n_connection) {
-    const n = 30
-    await wait(800)
+    const n = steps
+    await wait(500)
     let i1 = 0
     while (i1 < n) {
       const con = createSocketIOConnection(url, i1 + i, wsOptions)
@@ -54,4 +53,5 @@ async function wait(time) {
   })
 }
 
-test(n_connection)
+const n_connection = connectArgv || 10000 // 15000
+test(n_connection, 100)
